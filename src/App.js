94fd442efc3316bcf53f7442fd9c0d11a4d85cb3
hajadev <haja.dev@gmail.com>
2020-03-31 12:22:19 +0200

    Create card component and HallOfFame component with data list + Bugfix onclick on Card

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { Component } from 'react'
-import GuessCount from './GuessCount'
-import Card from './Card'
+import shuffle from 'lodash.shuffle'
+
 import './App.css'
 
-class App extends Component {
+import Card from './Card'
+import GuessCount from './GuessCount'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
 
-  handleCardClick(card){
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+
+class App extends Component {
+  cards = this.generateCards()
+
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  handleCardClick(card) {
     console.log(card, 'clicked')
   }
 
   render() {
     const won = new Date().getSeconds() % 2 === 0
+    
     return (
       <div className="memory">
         <GuessCount guesses={0} />
-        <Card card="😀" feedback="hidden" onClick={this.handleCardClick}/>
-        <Card card="🎉" feedback="justMatched" onClick={this.handleCardClick}/>
-        <Card card="💖" feedback="justMismatched" onClick={this.handleCardClick}/>
-        <Card card="🎩" feedback="visible" onClick={this.handleCardClick}/>
-        <Card card="🐶" feedback="hidden" onClick={this.handleCardClick}/>
-        <Card card="🐱" feedback="justMatched" onClick={this.handleCardClick}/>
-        {won && <p>GAGNÉ !!!</p>}
+        {this.cards.map((card, index) => (
+          <Card card={card} feedback="visible" key={index} onClick={this.handleCardClick}/>
+        ))}
+        {won && <HallOfFame entries={FAKE_HOF} />}
       </div>
     )
   }
